224a03636c59708e6c9973cde622ad314a85a02c
Jack Marquez <35302688+jackmarquez@users.noreply.github.com>
2021-12-16 17:00:50 -0400

    Update __manifest__.py

--- account_report_aged_receivable/__manifest__.py
+++ account_report_aged_receivable/__manifest__.py
@@ -21,7 +21,7 @@
     'category': 'Uncategorized',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
+    # any module necessary for this one to work correctly ..
     'depends': ['base','account_reports'],
 
     # always loaded
